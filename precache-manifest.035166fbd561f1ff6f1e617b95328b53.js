ee0bfd1cf29876e77bdb28b97524fca1cc0ccee4
Code Sandbox <hello@codesandbox.io>
2022-02-21 08:55:18 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.035166fbd561f1ff6f1e617b95328b53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65881fe0bfcfe8aea7a947bfd2da5dce",
+    "url": "https://amit-pep.github.io/csb-660ior/index.html"
+  },
+  {
+    "revision": "cfbd30ecb359469fc201",
+    "url": "https://amit-pep.github.io/csb-660ior/static/js/2.de3e90e7.chunk.js"
+  },
+  {
+    "revision": "c97857f556d846f1cc3458bc1f5b8e09",
+    "url": "https://amit-pep.github.io/csb-660ior/static/js/2.de3e90e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b615b2ecf87c416e81bb",
+    "url": "https://amit-pep.github.io/csb-660ior/static/js/main.2aa78957.chunk.js"
+  },
+  {
+    "revision": "6d9323da8a27223926fa",
+    "url": "https://amit-pep.github.io/csb-660ior/static/js/runtime-main.4f7065d3.js"
+  }
+]);
